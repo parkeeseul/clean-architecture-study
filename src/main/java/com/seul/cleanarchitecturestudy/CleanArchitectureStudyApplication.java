9da7e434aaf7828f90fba079c5e079e98ed2e605
seul <eeseul.p@gmail.com>
2021-12-18 13:23:28 +0900

    init

---
+++ src/main/java/com/seul/cleanarchitecturestudy/CleanArchitectureStudyApplication.java
@@ -0,0 +1,13 @@
+package com.seul.cleanarchitecturestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureStudyApplication.class, args);
+	}
+
+}
